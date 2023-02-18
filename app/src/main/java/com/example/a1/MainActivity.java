c383bfc03d4345ebbe0e43aa0810d7fa9e4bbe4b
l-Mel-l <constantine.dudorov@yandex.ru>
2023-02-18 11:25:05 +0300

    initial commit

---
+++ app/src/main/java/com/example/a1/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.a1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private Integer counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickBtnAddSon(View view) {
+        counter++;
+        TextView counterView = findViewById(R.id.txt_counter);
+        counterView.setText(counter.toString());
+    }
+
+
+}
